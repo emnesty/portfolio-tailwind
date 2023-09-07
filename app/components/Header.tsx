36abbf827ed42ab2eeb630f6f6117bccd07648e0
Luciano Silva <legostr@gmail.com>
2023-09-07 17:00:27 -0300

    Ajustes Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,29 +4,58 @@ import { Container, Flex, Text } from "@radix-ui/themes"
 import { NavigationMenu, NavigationMenuItem, NavigationMenuList } from "@radix-ui/react-navigation-menu"
 import { ThemeSwitcher } from "./ThemeSwitcher"
 import TranslationSwitcher from "./TranslationSwitcher"
-import TranslationText from "./TranslationText"
 import { useContext, createContext } from "react"
 import { LanguageContext } from "./LanguageContext"
 
-interface LanguageContextProps {
-  selectedText: {
-    HeaderMenu1: string
-    HeaderMenu2: string
-    HeaderMenu3: string
-  }
-}
+// interface LanguageContextProps {
+//   Header_pt: {
+//     HeaderMenu1: string
+//     HeaderMenu2: string
+//     HeaderMenu3: string
+//   }
+//   Header_en: {
+//     HeaderMenu1: string
+//     HeaderMenu2: string
+//     HeaderMenu3: string
+//   }
+//   Header_es: {
+//     HeaderMenu1: string
+//     HeaderMenu2: string
+//     HeaderMenu3: string
+//   }
+// }
 
 // Provide a default value when creating the context
-export const MyLanguageContext = createContext<LanguageContextProps>({
+// export const MyLanguageContext = createContext<LanguageContextProps>({
+//   Header_pt: {
+//     HeaderMenu1: "Sobre",
+//     HeaderMenu2: "Trabalhos",
+//     HeaderMenu3: "Contato",
+//   },
+//   Header_en: {
+//     HeaderMenu1: "Acerca",
+//     HeaderMenu2: "Trabajos",
+//     HeaderMenu3: "Contacto",
+//   },
+//   Header_es: {
+//     HeaderMenu1: "About",
+//     HeaderMenu2: "Cases",
+//     HeaderMenu3: "Contact",
+//   },
+// })
+
+const defaultContext = {
   selectedText: {
-    HeaderMenu1: "About",
-    HeaderMenu2: "Cases",
-    HeaderMenu3: "Contact",
+    Titulo: "Default Title",
+    SubTitulo: "Default Subtitle",
+    HeaderMenu1: "Default Subtitle",
+    HeaderMenu2: "Default Subtitle",
+    HeaderMenu3: "Default Subtitle",
   },
-})
+}
 
 export default function Header() {
-  const { selectedText } = useContext(MyLanguageContext)
+  const { selectedText } = useContext(LanguageContext) || defaultContext
   return (
     <header className="border-b dark:border-slate-800 border-slate-200 pb-6">
       <Container style={{ marginLeft: 15, marginRight: 15 }} mt="6">
--- app/components/LanguageContext.tsx
+++ app/components/LanguageContext.tsx
@@ -3,13 +3,22 @@ import React, { createContext, useState, useContext } from "react"
 export type Language = "en" | "es" | "pt"
 
 interface LanguageContextProps {
-  selectedText: { Titulo: string; SubTitulo: string; Heading: string; HeadingSubTitle: string; Hello: string }
+  selectedText: {
+    Titulo: string
+    SubTitulo: string
+    Heading: string
+    HeadingSubTitle: string
+    Hello: string
+    HeaderMenu1: string
+    HeaderMenu2: string
+    HeaderMenu3: string
+  }
   setSelectedLanguage: React.Dispatch<React.SetStateAction<Language>>
 }
 
 // Provide a default value
 const defaultContext: LanguageContextProps = {
-  selectedText: { Titulo: "", SubTitulo: "", Heading: "", HeadingSubTitle: "", Hello: "" },
+  selectedText: { Titulo: "", SubTitulo: "", Heading: "", HeadingSubTitle: "", Hello: "", HeaderMenu1: "," },
   setSelectedLanguage: () => {}, // Provide a no-op function as default
 }
 
